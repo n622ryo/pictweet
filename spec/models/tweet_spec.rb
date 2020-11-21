24cf9eb2756085cb134d511a8dbbd001a8631b66
n622ryo <fcv53@gmail.com>
2020-11-22 01:14:16 +0900

    エラーメッセージの日本語対応の実装

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -8,23 +8,30 @@ RSpec.describe Tweet, type: :model do
   describe 'ツイートの保存' do
     context "ツイートが保存できる場合" do
       it "画像とテキストがあればツイートは保存される" do
-        exoect(@tweet).to be_valid
-      end
-      it "テキストのみあればツイートは保存される" do
-        @tweet.image = ""
         expect(@tweet).to be_valid
       end
+
+      # it "テキストのみであればツイートは保存される" do
+      #   @tweet.image = ""
+      #   expect(@tweet).to be_valid
+      # end
     end
+
     context "ツイートが保存できない場合" do
       it "テキストがないとツイートは保存できない" do
         @tweet.text = ""
         @tweet.valid?
-        expect(@tweet.errors.full_messages).to include("Text can't be blank")
-      end     
+        expect(@tweet.errors.full_messages).to include("テキストを入力してください")
+      end
+      it "画像がないとツイートは保存できない" do
+        @tweet.image = nil
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include("画像を入力してください")
+      end
       it "ユーザーが紐付いていないとツイートは保存できない" do
         @tweet.user = nil
         @tweet.valid?
-        expect(@tweet.errors.full_messages).to include("User must exist")
+        expect(@tweet.errors.full_messages).to include("Userを入力してください")
       end
     end
   end
